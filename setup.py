ed835c053b4cbf7d54f419ef2e1902d0ecff7dbe
Christoph Gohlke <cgohlke@uci.edu>
2019-12-31 18:26:28 -0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 # oiffile/setup.py
 
 """Oiffile package setuptools script."""
@@ -12,18 +11,23 @@ with open('oiffile/oiffile.py') as fh:
     code = fh.read()
 
 version = re.search(r"__version__ = '(.*?)'", code).groups()[0]
-description = re.search(r'"""(.*)\.[\r\n?|\n]', code).groups()[0]
-readme = re.search(r'[\r\n?|\n]{2}"""(.*)"""[\r\n?|\n]{2}from', code,
-                   re.MULTILINE | re.DOTALL).groups()[0]
-license = re.search(r'(# Copyright.*?[\r\n?|\n])[\r\n?|\n]+""', code,
-                    re.MULTILINE | re.DOTALL).groups()[0]
+
+description = re.search(r'"""(.*)\.(?:\r\n|\r|\n)', code).groups()[0]
+
+readme = re.search(r'(?:\r\n|\r|\n){2}"""(.*)"""(?:\r\n|\r|\n){2}__version__',
+                   code, re.MULTILINE | re.DOTALL).groups()[0]
 
 readme = '\n'.join([description, '=' * len(description)]
                    + readme.splitlines()[1:])
+
+license = re.search(r'(# Copyright.*?(?:\r\n|\r|\n))(?:\r\n|\r|\n)+""', code,
+                    re.MULTILINE | re.DOTALL).groups()[0]
+
 license = license.replace('# ', '').replace('#', '')
 
 if 'sdist' in sys.argv:
     with open('LICENSE', 'w') as fh:
+        fh.write('BSD 3-Clause License\n\n')
         fh.write(license)
     with open('README.rst', 'w') as fh:
         fh.write(readme)
@@ -38,8 +42,8 @@ setup(
     url='https://www.lfd.uci.edu/~gohlke/',
     license='BSD',
     packages=['oiffile'],
-    python_requires='>=2.7',
-    install_requires=['numpy>=1.11.3', 'tifffile>=2019.1.1'],
+    python_requires='>=3.6',
+    install_requires=['numpy>=1.14.5', 'tifffile>=2019.1.1'],
     platforms=['any'],
     classifiers=[
         'Development Status :: 4 - Beta',
@@ -47,11 +51,9 @@ setup(
         'Intended Audience :: Science/Research',
         'Intended Audience :: Developers',
         'Operating System :: OS Independent',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3 :: Only',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
-        ],
+        'Programming Language :: Python :: 3.8',
+    ],
 )
