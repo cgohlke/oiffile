f31fdac9b6885a1dc2ce292505236fad09472845
Christoph Gohlke <cgohlke@cgohlke.com>
2023-08-30 09:50:52 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,8 +2,8 @@
 
 """Oiffile package setuptools script."""
 
-import sys
 import re
+import sys
 
 from setuptools import setup
 
@@ -16,15 +16,15 @@ def search(pattern, code, flags=0):
     return match.groups()[0]
 
 
-with open('oiffile/oiffile.py') as fh:
+with open('oiffile/oiffile.py', encoding='utf-8') as fh:
     code = fh.read()
 
-version = search(r"__version__ = '(.*?)'", code)
+version = search(r"__version__ = '(.*?)'", code).replace('.x.x', '.dev0')
 
 description = search(r'"""(.*)\.(?:\r\n|\r|\n)', code)
 
 readme = search(
-    r'(?:\r\n|\r|\n){2}"""(.*)"""(?:\r\n|\r|\n){2}[__version__|from]',
+    r'(?:\r\n|\r|\n){2}"""(.*)"""(?:\r\n|\r|\n){2}from __future__',
     code,
     re.MULTILINE | re.DOTALL,
 )
@@ -42,10 +42,10 @@ license = search(
 license = license.replace('# ', '').replace('#', '')
 
 if 'sdist' in sys.argv:
-    with open('LICENSE', 'w') as fh:
+    with open('LICENSE', 'w', encoding='utf-8') as fh:
         fh.write('BSD 3-Clause License\n\n')
         fh.write(license)
-    with open('README.rst', 'w') as fh:
+    with open('README.rst', 'w', encoding='utf-8') as fh:
         fh.write(readme)
 
 setup(
@@ -54,6 +54,7 @@ setup(
     license='BSD',
     description=description,
     long_description=readme,
+    long_description_content_type='text/x-rst',
     author='Christoph Gohlke',
     author_email='cgohlke@cgohlke.com',
     url='https://www.cgohlke.com',
@@ -63,8 +64,10 @@ setup(
         # 'Documentation': 'https://',
     },
     packages=['oiffile'],
-    python_requires='>=3.8',
-    install_requires=['numpy>=1.19.2', 'tifffile>=2021.11.2'],
+    package_data={'oiffile': ['py.typed']},
+    python_requires='>=3.9',
+    install_requires=['numpy', 'tifffile'],
+    extras_require={'all': ['matplotlib']},
     platforms=['any'],
     classifiers=[
         'Development Status :: 4 - Beta',
@@ -73,9 +76,9 @@ setup(
         'Intended Audience :: Developers',
         'Operating System :: OS Independent',
         'Programming Language :: Python :: 3 :: Only',
-        'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3.10',
         'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12',
     ],
 )
